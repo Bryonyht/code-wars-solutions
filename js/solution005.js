73a9c6e1788bd9e20951002ac700fb75318e33dc
Bryony Tayler <bryonyhazeltayler@gmail.com>
2023-03-17 10:38:01 +0000

    solution 005 complete- array ladder 02

---
+++ js/solution005.js
@@ -0,0 +1,14 @@
+
+// Complete the keysAndValues function so that it takes in an object and returns the keys and values as separate arrays.
+
+// Example:
+
+// keysAndValues({a: 1, b: 2, c: 3}) // should return [['a', 'b', 'c'], [1, 2, 3]]
+
+function keysAndValues(data){
+    let answer = []
+    answer[0] = Object.keys(data)
+    answer[1] =Object.values(data)
+
+    return answer
+}
