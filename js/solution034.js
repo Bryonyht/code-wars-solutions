a913c028bc88a4181780067e4f742c86d09919ff
Bryony Tayler <bryonyhazeltayler@gmail.com>
2023-04-19 10:12:01 +0100

    solution 34 complete

---
+++ js/solution034.js
@@ -0,0 +1,43 @@
+//Your task is to add a new property usersAnswer to every object in the array questions. The value of 
+//usersAnswer should be set to null. The solution should work for array of any length
+
+// parameter- an array of any length- inc 0?
+// return- array of objects with additional property
+// examples // 
+var questions = [{
+    question: "What's the currency of the USA?",
+    choices: ["US dollar", "Ruble", "Horses", "Gold"],
+    corAnswer: 0
+}, {
+    question: "Where was the American Declaration of Independence signed?",
+    choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
+    corAnswer: 0
+}]
+
+// //var questions = [{
+//     question: "What's the currency of the USA?",
+//     choices: ["US dollar", "Ruble", "Horses", "Gold"],
+//     corAnswer: 0,
+//     usersAnswer: null
+// }, {
+//     question: "Where was the American Declaration of Independence signed?",
+//     choices: ["Philadelphia", "At the bottom", "Frankie's pub", "China"],
+//     corAnswer: 0,
+//     usersAnswer: null
+// }]//
+//
+// loop through array
+// add new property to each object
+// return array
+
+
+function addUsersAnswer(arr){
+
+arr.map(x => x.usersAnswer = null)
+
+return arr
+
+}
+
+console.log(addUsersAnswer(questions))
+console.log(addUsersAnswer([]))
